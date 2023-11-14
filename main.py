b793b4e4e0591b3de5a020981329bd38005f4255
Olena Abert <oabert@gmail.com>
2023-11-14 12:44:03 -0500

    Add desc, images, url

--- main.py
+++ main.py
@@ -21,16 +21,21 @@ with container:
 
 sub_content = """Below you can find some of the apps I have build in Python."""
 st.write(sub_content)
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 data_frame = pandas.read_csv('data.csv', sep=';')
 
 with col3:
     for index, row in data_frame[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/'+row['image'])
+        st.write(f'[Source code]({row["url"]})')
 
-with (col4
-      ):
+
+with (col4):
     for index, row in data_frame[10:].iterrows():
         st.header(row['title'])
-
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f'[Source code]({row["url"]})')
